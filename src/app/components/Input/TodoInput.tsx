4cdd138417cf1baaa3daafcbe4f8631ebd66c459
Vespucci95 <donglonk@naver.com>
2023-10-21 22:12:30 +0900

    🎨 TodoInput forwardRef 적용

--- src/app/components/Input/TodoInput.tsx
+++ src/app/components/Input/TodoInput.tsx
@@ -1,18 +1,17 @@
 'use client'
-import React, { forwardRef, InputHTMLAttributes } from 'react';
-import { css } from '@emotion/react';
-
-interface Props extends InputHTMLAttributes<HTMLInputElement> {
+import React, { Attributes, forwardRef, InputHTMLAttributes } from 'react';
+import { css as style } from '@emotion/react';
+interface Props extends InputHTMLAttributes<HTMLInputElement>, Attributes {
     sortNum: string;
 }
 
-const inputWrapper = css`
-      display: flex;
-      flex-direction: row;
-    `;
-
-const TodoInput = forwardRef<HTMLInputElement, Props>(({sortNum, ...props}, ref) => (
-    <div css={inputWrapper}>
+const TodoInput = forwardRef<HTMLInputElement, Props>((
+    {
+        sortNum,
+        css = style`background: red; display: flex; flex-direction: row;`,
+        ...props
+    }, ref) => (
+    <div css={css}>
         {sortNum}
         <input ref={ref} {...props} />
     </div>
