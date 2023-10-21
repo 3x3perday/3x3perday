a887f45b8285ba85bdf82181c2d028d23e16d2eb
Vespucci95 <donglonk@naver.com>
2023-10-21 23:30:03 +0900

    ♻️ 함수명 변경

--- src/app/utils/dom.ts
+++ src/app/utils/dom.ts
@@ -1,6 +1,6 @@
 import { ChangeEvent } from 'react';
 
-export const inputHeightResize = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+export const resizeInputHeight = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     e.target.style.height = 'auto';
     e.target.style.height = e.target.scrollHeight + 'px';
 }
