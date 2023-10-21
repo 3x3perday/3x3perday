787ff15668c61c1a64840ce0b6b66bebc5137483
Hyunseo Lee <donglonk@naver.com>
2023-10-21 23:46:04 +0900

    ✨TodoTextArea Component 추가 (#3)
    * 🙈 ignore xml file
    
    * ✨ TodoInput Component 추가
    
    * 🎨 TodoInput forwardRef 적용
    
    * 🚚 TodoInput -> TodoTextArea
    
    * 💄 TodoTextArea Style 추가
    
    * 💄 border 삭제
    
    * ✨ input 높이창 조절 event
    
    * ♻️ 함수명 변경
    
    * 🎨 isResizeHeight Property 추가
    
    * ✨ textArea 높이에 따라 크기 조정 추가
    
    * 🔥 미사용코드 제거

---
+++ src/app/components/Input/TodoTextArea.tsx
@@ -0,0 +1,53 @@
+'use client'
+import React, { Attributes, ChangeEvent, forwardRef, InputHTMLAttributes } from 'react';
+import { css as style } from '@emotion/react';
+import { resizeInputHeight } from '@/app/utils/dom';
+interface Props extends InputHTMLAttributes<HTMLTextAreaElement>, Attributes {
+    width?: number;
+    isResizeHeight?: boolean;
+}
+
+const TodoTextArea = forwardRef<HTMLTextAreaElement, Props>((
+    {
+        width = 350,
+        isResizeHeight = true,
+        css = style`
+            ${isResizeHeight && `
+                -ms-overflow-style: none; /* IE and Edge */
+                scrollbar-width: none; /* Firefox */
+                ::-webkit-scrollbar {
+                    display: none; /* Chrome, Safari, Opera*/
+                }
+            `}
+            
+            text-transform: uppercase;
+            font-size: 100px;
+            display: flex;
+            flex-direction: row;
+            background-color: transparent;
+            width: ${width}px;
+            resize: none;
+            outline: none;
+            border: 0;  
+        `,
+        onChange,
+        ...props
+    }, ref) => {
+    const onChangeTodo = (e:ChangeEvent<HTMLTextAreaElement>) => {
+        if(isResizeHeight) {
+            resizeInputHeight(e);
+        }
+        onChange && onChange(e);
+    }
+    return (
+        <textarea
+            ref={ref}
+            rows={1}
+            onChange={onChangeTodo}
+            css={css}
+            {...props}
+        />
+    )
+})
+
+export default TodoTextArea
