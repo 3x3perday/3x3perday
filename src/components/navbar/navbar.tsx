c3e1b7ae9f282fead2834f579fa4b48903a300d8
임현홍 <bbnerino@gmail.com>
2023-10-23 00:02:44 +0900

    💄 navbar design

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -33,39 +33,94 @@ export default function Navbar({ date, setDate }: Props) {
   };
 
   return (
-    <div css={styles}>
-      {isToday && (
-        <Image
-          onClick={goYesterday}
-          src="/icon/arrow_left.png"
-          width={20}
-          height={25}
-          alt="arrow"
-          className="arrow_left"
-        />
-      )}
-      <div className="date">{date}</div>
-      {!isToday && (
-        <Image
-          onClick={goToday}
-          src="/icon/arrow_right.png"
-          width={20}
-          height={25}
-          alt="arrow"
-          className="arrow_right"
-        />
-      )}
+    <div css={navBarCSS}>
+      <div css={navTopCSS}>
+        <div className="left">
+          <Image
+            src="/image/logo.png"
+            width={120}
+            height={60}
+            alt="logo"
+            className="logo"
+          />
+        </div>
+        <div className="right">
+          <div className="profile">
+            <Image
+              src="/image/mimoji.png"
+              width={50}
+              height={50}
+              alt="setting"
+              className="setting"
+            />
+          </div>
+        </div>
+      </div>
+      <div css={dateCSS}>
+        {isToday && (
+          <Image
+            onClick={goYesterday}
+            src="/icon/arrow_left.png"
+            width={15}
+            height={20}
+            alt="arrow"
+            className="arrow_left"
+          />
+        )}
+        <div className="date">{date}</div>
+        {!isToday && (
+          <Image
+            onClick={goToday}
+            src="/icon/arrow_right.png"
+            width={15}
+            height={20}
+            alt="arrow"
+            className="arrow_right"
+          />
+        )}
+      </div>
     </div>
   );
 }
 
-const styles = css`
+const navBarCSS = css`
+  display: flex;
+  height: 160px;
+  background-color: #292929;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const navTopCSS = css`
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  .left {
+    padding: 20px;
+  }
+  .right {
+    display: flex;
+    align-items: center;
+    padding-right: 20px;
+    .profile {
+      background-color: #fff;
+      height: 60px;
+      width: 60px;
+      border-radius: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+    }
+  }
+`;
+
+const dateCSS = css`
   display: flex;
   justify-content: center;
   align-items: center;
   position: relative;
   height: 60px;
-  background-color: #3a3a39;
   .arrow_left {
     cursor: pointer;
     position: absolute;
