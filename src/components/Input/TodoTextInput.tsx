6918752f00dae145fb82acd6d8da37d5e3fa6b11
Vespucci95 <donglonk@naver.com>
2023-10-23 04:40:20 +0900

    🎨 마크업 수정

--- src/components/Input/TodoTextInput.tsx
+++ src/components/Input/TodoTextInput.tsx
@@ -3,14 +3,20 @@
 import React, { DOMAttributes, InputHTMLAttributes } from 'react';
 import { css } from "@emotion/react";
 import { TodoTextArea } from '@/components/Input/TodoTextArea';
+import { Icon } from '@/components/Icon/Icon';
+import FingerButton from '@/components/Button/FingerButton';
+import { Button } from '@/components/Button/Button';
 
 export interface TodoTextInputProps {
+    isMainTodo?: boolean;
     value?: string;
     onChange?: InputHTMLAttributes<HTMLTextAreaElement>['onChange'];
     onClickToggle?: DOMAttributes<HTMLButtonElement>['onChange'];
-    prefixText?: string;
+    prefixTodoNumber?: 1 | 2 | 3;
     visibleToggleBtn?: boolean;
     editable?: boolean;
+    checked?: boolean;
+    onClickCheck?: () => void;
 }
 
 /**
@@ -18,26 +24,55 @@ export interface TodoTextInputProps {
  * @param value Todo 입력 값
  * @param onChange Todo 변경 이벤트
  * @param onClickToggle 토글 버튼 클릭 이벤트
- * @param prefixText
+ * @param prefixTodoNumber
  * @param visibleToggleBtn 토글 버튼을 보이거나 숨깁니다.
  */
 
 export const TodoTextInput = (
     {
+        isMainTodo = true,
         value,
         onClickToggle,
         onChange,
-        prefixText = "1.",
+        prefixTodoNumber = 1,
         visibleToggleBtn = false,
-        editable = true
+        editable = true,
+        checked = false,
+        onClickCheck
     }: TodoTextInputProps) => {
     return (
         <div css={todoContainerCSS}>
-            <div css={prefixWrapperCSS}>
-                <p css={prefixFontStyle}>
-                    {prefixText}
-                </p>
-                {visibleToggleBtn && <button onClick={onClickToggle} css={toggleButtonCSS} />}
+            <div>
+                {
+                    isMainTodo ? (
+                        <div css={mainTodoIconWrapper}>
+                            {
+                                !checked ? (
+                                    <FingerButton
+                                        onClick={onClickCheck}
+                                        fingerCount={prefixTodoNumber}
+                                        isActive={visibleToggleBtn ?? false}
+                                    />
+                                ) : (
+                                    <Button
+                                        onClick={onClickCheck}
+                                        css={css`position: absolute; left: -52px;`}
+                                    >
+                                        <Icon
+                                            name={'thumbs-up'}
+                                            width={'152px'}
+                                            height={'144px'}
+                                        />
+                                    </Button>
+                                )
+                            }
+                        </div>
+                    ) : (
+                        <p css={prefixFontStyle}>
+                            {prefixTodoNumber}
+                        </p>
+                    )
+                }
             </div>
             <div>
                 <TodoTextArea
@@ -46,12 +81,14 @@ export const TodoTextInput = (
                     onChange={onChange}
                     css={todoTextAreaCSS}
                 />
+                {visibleToggleBtn && <button onClick={onClickToggle} css={toggleButtonCSS} />}
             </div>
         </div>
     );
 }
 const prefixFontStyle = css`
   width: 90px;
+  font-family: Pretendard, sans-serif;
   font-size: 6.25rem;;
   font-weight: 900;
   color: white;
@@ -59,12 +96,13 @@ const prefixFontStyle = css`
 const todoContainerCSS = css`
   position: relative;
   display: flex;
+  gap: 16px;
 `;
-const prefixWrapperCSS = css`
+const mainTodoIconWrapper = css`
+  width: 93px;
   display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: flex-end;
+  align-items: center;
+  height: 100%;
 `;
 const toggleButtonCSS = css`
   width: 20px;
