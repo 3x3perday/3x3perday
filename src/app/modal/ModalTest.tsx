51fcea3ba476aa3adca5f70084fc3871dc76cb78
임현홍 <83864924+bbnerino@users.noreply.github.com>
2023-10-22 16:58:54 +0900

    ✨ Add modal function   (#7)

---
+++ src/app/modal/ModalTest.tsx
@@ -0,0 +1,9 @@
+import Modal from "@/components/modal/modal";
+import { ModalProps } from "@/components/modal/util";
+import React from "react";
+
+const ModalTest = (props: ModalProps) => {
+  return <Modal {...props}>ModalTest</Modal>;
+};
+
+export default ModalTest;
