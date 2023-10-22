9ccddda3ff03d94e4a17e4f53c1a56b842026b3a
Vespucci95 <donglonk@naver.com>
2023-10-22 19:24:32 +0900

    🎨 subTodo 구조 설정

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,28 +1,50 @@
 'use client';
 
 import {css} from "@emotion/react";
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import Navbar from "@/components/navbar";
 import { TodoTextInput } from '@/components/Input';
 
+interface Todo3x3Model {
+	mainTodo: string;
+	subTodos: string[];
+}
+
 export default function Home() {
-	const [todo, setTodo] = useState("");
-	const [todo2, setTodo2] = useState("");
+	const [todos, setTodos] = useState<Todo3x3Model>({
+		mainTodo: "",
+		subTodos: []
+	});
+	const onChangeMainTodo = (e: ChangeEvent<HTMLTextAreaElement>) => {
+		setTodos(prevTodo => ({
+			...prevTodo,
+			mainTodo: e.target.value
+		}))
+	}
+	const onChangeSubTodo = (num: number) => (e: ChangeEvent<HTMLTextAreaElement>) => {
+		setTodos(prevTodo => ({
+			...prevTodo,
+			subTodos: prevTodo.subTodos.map((todo, i) => i === num ? e.target.value : todo)
+		}))
+	}
 	return (
 		<main css={inner}>
 			<Navbar />
 			<TodoTextInput
-				value={todo}
-				onChange={e => setTodo(e.target.value)}
+				value={todos.mainTodo}
+				onChange={onChangeMainTodo}
 				onClickToggle={e => console.log(e)}
 			/>
-			<TodoTextInput
-				prefixText={"2."}
-				value={todo2}
-				onChange={e => setTodo2(e.target.value)}
-				onClickToggle={e => console.log(e)}
-				visibleToggleBtn
-			/>
+			{
+				todos.subTodos.map((todo, i) => (
+					<TodoTextInput
+						prefixText={`${i + 1}.`}
+						value={todos.subTodos[i]}
+						onChange={onChangeSubTodo(i)}
+						onClickToggle={e => console.log(e)}
+					/>
+				))
+			}
 		</main>
 	);
 }
