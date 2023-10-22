b936244013075c8640d829ab104dabb828b62954
Vespucci95 <donglonk@naver.com>
2023-10-22 16:46:00 +0900

    ♻️ inline style 제거

--- src/components/Input/TodoTextArea.tsx
+++ src/components/Input/TodoTextArea.tsx
@@ -7,34 +7,37 @@ interface Props extends InputHTMLAttributes<HTMLTextAreaElement>, Attributes {
     isResizeHeight?: boolean;
 }
 
+const invisibleScroll = `
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+    ::-webkit-scrollbar {
+        display: none; /* Chrome, Safari, Opera*/
+    }
+`;
+const textStyle = `
+    text-transform: uppercase;
+    font-size: 6.25rem;
+    display: flex;
+    flex-direction: row;
+    background-color: transparent;
+    resize: none;
+    outline: none;
+    border: 0;
+`;
 const TodoTextArea = forwardRef<HTMLTextAreaElement, Props>((
     {
         width = '100%',
         isResizeHeight = true,
         css = style`
-            ${isResizeHeight && `
-                -ms-overflow-style: none; /* IE and Edge */
-                scrollbar-width: none; /* Firefox */
-                ::-webkit-scrollbar {
-                    display: none; /* Chrome, Safari, Opera*/
-                }
-            `}
-            
-            text-transform: uppercase;
-            font-size: 6.25rem;
-            display: flex;
-            flex-direction: row;
-            background-color: transparent;
+            ${isResizeHeight && invisibleScroll}
+            ${textStyle}
             width: ${width};
-            resize: none;
-            outline: none;
-            border: 0;  
         `,
         onChange,
         ...props
     }, ref) => {
-    const onChangeTodo = (e:ChangeEvent<HTMLTextAreaElement>) => {
-        if(isResizeHeight) {
+    const onChangeTodo = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        if (isResizeHeight) {
             resizeInputHeight(e);
         }
         onChange && onChange(e);
