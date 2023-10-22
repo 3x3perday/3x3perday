dd1334d55c11e409e6157d67a52a2986405a27e9
임현홍 <bbnerino@gmail.com>
2023-10-22 23:00:00 +0900

    🔥 modal 페이지에서 todo 기능 삭제

--- src/app/modal/page.tsx
+++ src/app/modal/page.tsx
@@ -1,43 +1,22 @@
 "use client";
 
 import ModalModal from "./components/modalmodal";
-import React, { useState } from "react";
+import React from "react";
 import DNDPage from "./components/DNDPage";
 import { css } from "@emotion/react";
-import Navbar from "@/components/navbar/navbar";
 
 const ModalPage = () => {
   // --- Draggable이 Droppable로 드래그 되었을 때 실행되는 이벤트
-  const [date, setDate] = useState(new Date());
 
   return (
     <main>
       <div css={CSS}>
-        <Navbar date={date} />
         <ModalModal />
         <DNDPage />
       </div>
     </main>
   );
 };
-
-const data = [
-  {
-    date: "2021-09-01",
-    mainTodo: [
-      {
-        title: "title",
-        isDone: false,
-        subTodo: [
-          {
-            title: "subTitle",
-            isDone: false,
-          },
-        ],
-      },
-    ],
-  },
-];
 
 const CSS = css`
   background-color: #fff;
