459ca179efbd3e4c616a758207f51bb60c1d4b80
Vespucci95 <donglonk@naver.com>
2023-10-22 20:27:14 +0900

    ♻️ Todo Component 분리

---
+++ src/components/Item/Todo.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { TodoTextInput, TodoTextInputProps } from '@/components/Input';
+import { css } from '@emotion/react';
+import React from 'react';
+
+export interface Todo3x3Model {
+    id: number;
+    mainTodo: string;
+    subTodos: string[];
+}
+
+interface TodoProps extends Todo3x3Model {
+    visibleToggleBtn?: TodoTextInputProps['visibleToggleBtn'];
+    onChangeMainTodo: (e: React.ChangeEvent<HTMLTextAreaElement>, mainTodoId: number) => void;
+    onChangeSubTodo: (e: React.ChangeEvent<HTMLTextAreaElement>, mainTodoId: number, subTodoId: number) => void;
+    subTodoMaxLength?: number;
+    onClickAddSubTodo?: (id: number) => void;
+}
+
+export const Todo = (
+    {
+        id,
+        mainTodo,
+        subTodos,
+        subTodoMaxLength = 3,
+        visibleToggleBtn = false,
+        onChangeMainTodo,
+        onChangeSubTodo,
+        onClickAddSubTodo,
+    }: TodoProps) => {
+    return (
+        <div>
+            <TodoTextInput
+                prefixText={`${id + 1}.`}
+                value={mainTodo}
+                onChange={e => onChangeMainTodo(e, id)}
+                visibleToggleBtn={visibleToggleBtn}
+                onClickToggle={e => console.log(e)}
+            />
+            {
+                subTodos.map((subTodo, subTodoId) => (
+                    <div
+                        key={subTodoId}
+                        css={css`margin-left: 20px`}
+                    >
+                        <TodoTextInput
+                            prefixText={`${subTodoId + 1})`}
+                            value={subTodo[subTodoId]}
+                            onChange={e => onChangeSubTodo && onChangeSubTodo(e, id, subTodoId)}
+                            onClickToggle={e => console.log(e)}
+                        />
+                    </div>
+                ))
+            }
+            <button
+                onClick={() => onClickAddSubTodo && onClickAddSubTodo(id)}
+                css={addButtonCSS(subTodos.length, subTodoMaxLength)}
+            >+
+            </button>
+        </div>
+    )
+}
+
+const addButtonCSS = (todoLength: number, maxLength: number) => css`
+  display: ${todoLength === maxLength ? 'none' : 'block'};
+  width: 30px;
+  height: 30px;
+  margin-left: 20px;
+`;
