33f90ce7b7ea30dcdce11a905fd79472b67db776
tankboyy <76859838+tankboyy@users.noreply.github.com>
2023-10-23 21:30:53 +0900

    ✨ todapage css 변경 (#32)
    todopage css를 피그마와 같게 수정했습니다.

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,228 +1,228 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import {
-  mockTodoData,
-  mocktodos,
-  Todo3x3Model,
-  TodoPageModel,
+	mockTodoData,
+	mocktodos,
+	Todo3x3Model,
+	TodoPageModel,
 } from "@/types/todo";
-import { Date } from "@/utils/date";
-import { todo } from "@/utils/todo";
+import {Date} from "@/utils/date";
+import {todo} from "@/utils/todo";
 import Navbar from "@/components/navbar/navbar";
-import { css } from "@emotion/react";
-import { TodoItem } from "@/components/Item/TodoItem";
-import { MainTodo } from "@/components/Todo/MainTodo";
-import { Icon } from "@/components/Icon/Icon";
+import {css} from "@emotion/react";
+import {TodoItem} from "@/components/Item/TodoItem";
+import {MainTodo} from "@/components/Todo/MainTodo";
+import {Icon} from "@/components/Icon/Icon";
 import {
-  DragDropContext,
-  Draggable,
-  DropResult,
-  Droppable,
+	DragDropContext,
+	Draggable,
+	DropResult,
+	Droppable,
 } from "@hello-pangea/dnd";
 import Image from "next/image";
-import { useDnD } from "@/utils/dnd";
+import {useDnD} from "@/utils/dnd";
 import styled from "@emotion/styled";
-import { useRouter } from 'next/navigation';
+import {useRouter} from 'next/navigation';
 
 
 interface TodoView extends Todo3x3Model {
-  visibleSubTodo: boolean;
+	visibleSubTodo: boolean;
 }
 
 const convertTodoView = (todos: Todo3x3Model[]): TodoView[] => {
-  return todos.map((todo) => ({
-    ...todo,
-    visibleSubTodo: false,
-  }));
+	return todos.map((todo) => ({
+		...todo,
+		visibleSubTodo: false,
+	}));
 };
 
 export default function Home() {
-  const router = useRouter();
-  const [entireTodos, setEntireTodos] = useState<TodoPageModel[]>(mockTodoData); // 전체 데이터
-  const [todoPage, setTodoPage] = useState<TodoPageModel>(mocktodos); // 오늘의 데이터
+	const router = useRouter();
+	const [entireTodos, setEntireTodos] = useState<TodoPageModel[]>(mockTodoData); // 전체 데이터
+	const [todoPage, setTodoPage] = useState<TodoPageModel>(mocktodos); // 오늘의 데이터
 
-  const [date, setDate] = useState(Date.getToday()); // 날짜
-  const [todos, setTodos] = useState<TodoView[]>(
-    convertTodoView(mocktodos.todos)
-  );
+	const [date, setDate] = useState(Date.getToday()); // 날짜
+	const [todos, setTodos] = useState<TodoView[]>(
+		convertTodoView(mocktodos.todos)
+	);
 
-  const onClickTodo = (todoId: number) => () => {
-    router.push("/todo/edit");
-  };
+	const onClickTodo = (todoId: number) => () => {
+		router.push("/todo/edit");
+	};
 
-  const onClickToggle = (mainTodoId: number, state: boolean) => {
-    setTodos((prevState) =>
-      prevState.map((itemm) => {
-        if (itemm.id === mainTodoId) {
-          itemm.visibleSubTodo = state;
-        }
-        return itemm;
-      })
-    );
-  };
+	const onClickToggle = (mainTodoId: number, state: boolean) => {
+		setTodos((prevState) =>
+			prevState.map((itemm) => {
+				if (itemm.id === mainTodoId) {
+					itemm.visibleSubTodo = state;
+				}
+				return itemm;
+			})
+		);
+	};
 
-  const onClickCheckMainTodo = (todoId: number) => () => {
-    setTodos((prevState) =>
-      prevState.map((item) => {
-        if (item.id === todoId) {
-          item.mainTodo.done = !item.mainTodo.done;
-        }
-        return item;
-      })
-    );
-  };
-  const onClickCheckSubTodo = (mainTodoId: number, subTodoId: number) => {
-    const receiveTodos: TodoView[] = [...todos];
-    receiveTodos[mainTodoId].subTodos = receiveTodos[mainTodoId].subTodos.map(
-      (todo, i) => {
-        if (i === subTodoId) {
-          return {
-            ...todo,
-            done: !todo.done,
-          };
-        }
-        return todo;
-      }
-    );
-    setTodos(receiveTodos);
-  };
+	const onClickCheckMainTodo = (todoId: number) => () => {
+		setTodos((prevState) =>
+			prevState.map((item) => {
+				if (item.id === todoId) {
+					item.mainTodo.done = !item.mainTodo.done;
+				}
+				return item;
+			})
+		);
+	};
+	const onClickCheckSubTodo = (mainTodoId: number, subTodoId: number) => {
+		const receiveTodos: TodoView[] = [...todos];
+		receiveTodos[mainTodoId].subTodos = receiveTodos[mainTodoId].subTodos.map(
+			(todo, i) => {
+				if (i === subTodoId) {
+					return {
+						...todo,
+						done: !todo.done,
+					};
+				}
+				return todo;
+			}
+		);
+		setTodos(receiveTodos);
+	};
 
-  useEffect(() => {
-    // 찾는 날짜가 전체 데이터에 있는지 확인
-    const _todos = entireTodos.find((entireTodo) => entireTodo.date === date);
-    if (_todos) return setTodoPage(_todos);
+	useEffect(() => {
+		// 찾는 날짜가 전체 데이터에 있는지 확인
+		const _todos = entireTodos.find((entireTodo) => entireTodo.date === date);
+		if (_todos) return setTodoPage(_todos);
 
-    // 찾는 날짜가 전체 데이터에 없으면 새로운 데이터를 만들어서 전체 데이터에 추가
-    const newEntireTodos = todo.getTodosWithNew(entireTodos, date);
-    setEntireTodos(newEntireTodos);
+		// 찾는 날짜가 전체 데이터에 없으면 새로운 데이터를 만들어서 전체 데이터에 추가
+		const newEntireTodos = todo.getTodosWithNew(entireTodos, date);
+		setEntireTodos(newEntireTodos);
 
-    // 전체 데이터에서 찾은 날짜의 데이터를 todoPage 에 넣어준다.
-    const _newTodos = newEntireTodos.find(
-      (entireTodo) => entireTodo.date === date
-    );
+		// 전체 데이터에서 찾은 날짜의 데이터를 todoPage 에 넣어준다.
+		const _newTodos = newEntireTodos.find(
+			(entireTodo) => entireTodo.date === date
+		);
 
-    if (_newTodos) setTodoPage(_newTodos);
-  });
+		if (_newTodos) setTodoPage(_newTodos);
+	});
 
-  //================================================================================================
-  useDnD();
-  const onDragEnd = ({ source, destination }: DropResult) => {
-    if (!destination) return;
-    const _items = JSON.parse(JSON.stringify(todos)) as typeof todos;
+	//================================================================================================
+	useDnD();
+	const onDragEnd = ({source, destination}: DropResult) => {
+		if (!destination) return;
+		const _items = JSON.parse(JSON.stringify(todos)) as typeof todos;
 
-    const [targetItem] = _items.splice(source.index, 1);
+		const [targetItem] = _items.splice(source.index, 1);
 
-    _items.splice(destination.index, 0, targetItem);
+		_items.splice(destination.index, 0, targetItem);
 
-    setTodos(_items);
-  };
+		setTodos(_items);
+	};
 
-  useEffect(() => {
-    if (entireTodos.length === 0) {
-      const data = localStorage.getItem("todos");
+	useEffect(() => {
+		if (entireTodos.length === 0) {
+			const data = localStorage.getItem("todos");
 
-      if (data) {
-        setEntireTodos(JSON.parse(data));
-      } else setEntireTodos(mockTodoData);
+			if (data) {
+				setEntireTodos(JSON.parse(data));
+			} else setEntireTodos(mockTodoData);
 
-      return;
-    }
-    localStorage.setItem("todos", JSON.stringify(entireTodos));
-  }, [entireTodos]);
+			return;
+		}
+		localStorage.setItem("todos", JSON.stringify(entireTodos));
+	}, [entireTodos]);
 
-  useEffect(() => {
-    const newTodos = entireTodos.map((data) => {
-      if (data.date !== date) return data;
+	useEffect(() => {
+		const newTodos = entireTodos.map((data) => {
+			if (data.date !== date) return data;
 
-      return { ...data, todos };
-    });
-    setEntireTodos(newTodos);
-    console.log("todos", newTodos);
-  }, [todos]);
+			return {...data, todos};
+		});
+		setEntireTodos(newTodos);
+		console.log("todos", newTodos);
+	}, [todos]);
 
 
-  // =================================================================================================
-  return (
-    <main
-      css={css`
+	// =================================================================================================
+	return (
+		<main
+			css={css`
         background-color: #292929;
-      `}
-    >
-      <Navbar date={date} setDate={setDate} />
-      <div css={inner}>
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="droppable">
-            {(provided) => (
-              <div ref={provided.innerRef} {...provided.droppableProps}>
-                {todos.map((todo, index) => (
-                  <Draggable
-                    key={todo.id}
-                    draggableId={String(todo.id)}
-                    index={index}
-                  >
-                    {(provided) => (
-                      <div
-                        key={todo.id}
-                        css={css`
+			`}
+		>
+			<Navbar date={date} setDate={setDate}/>
+			<div css={inner}>
+				<DragDropContext onDragEnd={onDragEnd}>
+					<Droppable droppableId="droppable">
+						{(provided) => (
+							<div ref={provided.innerRef} {...provided.droppableProps}>
+								{todos.map((todo, index) => (
+									<Draggable
+										key={todo.id}
+										draggableId={String(todo.id)}
+										index={index}
+									>
+										{(provided) => (
+											<div
+												key={todo.id}
+												css={css`
                           margin-bottom: 32px;
                           width: 344px;
                           position: relative;
-                        `}
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                      >
-                        <DragButton {...provided.dragHandleProps}>
-                          <Image
-                            src="/icon/drag_icon.png"
-                            width={15}
-                            height={15}
-                            alt="drag"
-                          />
-                        </DragButton>
-                        {todo.mainTodo.content !== "" ? (
-                          <TodoItem
-                            id={todo.id}
-                            mainTodo={todo.mainTodo}
-                            subTodos={todo.subTodos}
-                            visibleSubTodo={todo.visibleSubTodo}
-                            onClickToggle={onClickToggle}
-                            onClickCheckMainTodo={onClickCheckMainTodo(todo.id)}
-                            onClickCheckSubTodo={onClickCheckSubTodo}
-                            editable={false}
-                          />
-                        ) : (
-                          <Icon
-                            name={"plus"}
-                            width={"344px"}
-                            height={"169px"}
-                            onClick={onClickTodo(todo.id)}
-                          >
-                            <MainTodo
-                              prefixTodoNumber={todo.id + 1}
-                              value={todo.mainTodo.content}
-                              editable={false}
-                            />
-                          </Icon>
-                        )}
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </div>
-    </main>
-  );
+												`}
+												ref={provided.innerRef}
+												{...provided.draggableProps}
+											>
+												<DragButton {...provided.dragHandleProps}>
+													<Image
+														src="/icon/drag_icon.png"
+														width={15}
+														height={15}
+														alt="drag"
+													/>
+												</DragButton>
+												{todo.mainTodo.content !== "" ? (
+													<TodoItem
+														id={todo.id}
+														mainTodo={todo.mainTodo}
+														subTodos={todo.subTodos}
+														visibleSubTodo={todo.visibleSubTodo}
+														onClickToggle={onClickToggle}
+														onClickCheckMainTodo={onClickCheckMainTodo(todo.id)}
+														onClickCheckSubTodo={onClickCheckSubTodo}
+														editable={false}
+													/>
+												) : (
+													<Icon
+														name={"plus"}
+														width={"344px"}
+														height={"169px"}
+														onClick={onClickTodo(todo.id)}
+													>
+														<MainTodo
+															prefixTodoNumber={todo.id + 1}
+															value={todo.mainTodo.content}
+															editable={false}
+														/>
+													</Icon>
+												)}
+											</div>
+										)}
+									</Draggable>
+								))}
+								{provided.placeholder}
+							</div>
+						)}
+					</Droppable>
+				</DragDropContext>
+			</div>
+		</main>
+	);
 }
 const inner = css`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 10px 20px;
+  padding: 35px 20px;
 `;
 
 const DragButton = styled.div`
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,121 +1,128 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import Image from "next/image";
-import { css } from "@emotion/react";
+import {css} from "@emotion/react";
 import dayjs from "dayjs";
-import { Date } from "@/utils/date";
-import { usePathname, useRouter } from "next/navigation";
+import {Date} from "@/utils/date";
+import {usePathname, useRouter} from "next/navigation";
 
 interface Props {
-  date: string;
-  setDate: React.Dispatch<React.SetStateAction<string>>;
+	date: string;
+	setDate: React.Dispatch<React.SetStateAction<string>>;
 }
+
 // Navbar 는 날짜 관련 컴포넌트
-export default function Navbar({ date, setDate }: Props) {
-  const [isToday, setIsToday] = useState(true);
-  const router = useRouter();
-  useEffect(() => {
-    const today = Date.getToday();
-    if (date === today) {
-      return setIsToday(true);
-    }
-    setIsToday(false);
-  }, [date]);
+export default function Navbar({date, setDate}: Props) {
+	const [isToday, setIsToday] = useState(true);
+	const router = useRouter();
+	useEffect(() => {
+		const today = Date.getToday();
+		if (date === today) {
+			return setIsToday(true);
+		}
+		setIsToday(false);
+	}, [date]);
 
-  const goYesterday = () => {
-    const yesterday = Date.getYesterday(date);
-    setDate(yesterday);
-  };
+	const goYesterday = () => {
+		const yesterday = Date.getYesterday(date);
+		setDate(yesterday);
+	};
 
-  const goToday = () => {
-    const today = Date.getToday();
-    setDate(today);
-  };
+	const goToday = () => {
+		const today = Date.getToday();
+		setDate(today);
+	};
 
-  const goAchieve = () => {
-    router.push("/achieve");
-  };
+	const goAchieve = () => {
+		router.push("/achieve");
+	};
 
-  const goEdit = () => {
-    router.push("/todo/edit");
-  };
-  const path = usePathname();
-  return (
-    <div css={navBarCSS}>
-      <div css={navTopCSS}>
-        <div className="left">
-          <Image
-            src="/image/logo.png"
-            width={120}
-            height={60}
-            alt="logo"
-            className="logo"
-          />
-        </div>
-        <div className="right">
-          <div className="profile">
-            <Image
-              src="/image/mimoji.png"
-              width={50}
-              height={50}
-              onClick={goAchieve}
-              alt="setting"
-              className="setting"
-            />
-          </div>
-        </div>
-      </div>
-      <div css={dateCSS}>
-        {isToday && (
-          <Image
-            onClick={goYesterday}
-            src="/icon/arrow_left.png"
-            width={15}
-            height={20}
-            alt="arrow"
-            className="arrow_left"
-          />
-        )}
-        <div className="date">{date}</div>
-        {!isToday && (
-          <Image
-            onClick={goToday}
-            src="/icon/arrow_right.png"
-            width={15}
-            height={20}
-            alt="arrow"
-            className="arrow_right"
-          />
-        )}
-      </div>
-      {path === "/todo" && (
-        <div css={btnCSS}>
-          <button onClick={goEdit} className="edit_btn">
-            수 정
-          </button>
-        </div>
-      )}
-    </div>
-  );
+	const goEdit = () => {
+		router.push("/todo/edit");
+	};
+	const path = usePathname();
+	return (
+		<div css={navBarCSS}>
+			<div css={navTopCSS}>
+				<div className="left">
+					<Image
+						src="/image/logo.svg"
+
+						width={90}
+						height={60}
+						alt="logo"
+						className="logo"
+					/>
+				</div>
+				<div className="right">
+					<div className="profile">
+						<Image
+							src="/image/mimoji.png"
+							width={50}
+							height={50}
+							onClick={goAchieve}
+							alt="setting"
+							className="setting"
+						/>
+					</div>
+				</div>
+			</div>
+			<div css={dateCSS}>
+				{isToday && (
+					<Image
+						onClick={goYesterday}
+						src="/icon/arrow_left.png"
+						width={15}
+						height={20}
+						alt="arrow"
+						className="arrow_left"
+					/>
+				)}
+				<div className="date">{date}</div>
+				{!isToday && (
+					<Image
+						onClick={goToday}
+						src="/icon/arrow_right.png"
+						width={15}
+						height={20}
+						alt="arrow"
+						className="arrow_right"
+					/>
+				)}
+				{path === "/todo" && (
+					<div css={btnCSS}>
+						<button onClick={goEdit} className="edit_btn">
+							수정
+						</button>
+					</div>
+				)}
+			</div>
+
+		</div>
+	);
 }
 
 const navBarCSS = css`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  padding-top: 36px;
 `;
 
 const navTopCSS = css`
   height: 100px;
   display: flex;
   justify-content: space-between;
+
   .left {
-    padding: 20px;
+    padding: 20px 20px 20px 35px;
   }
+
   .right {
     display: flex;
     align-items: center;
-    padding-right: 20px;
+    padding-right: 35px;
+
     .profile {
       background-color: #fff;
       height: 60px;
@@ -135,21 +142,22 @@ const dateCSS = css`
   align-items: center;
   position: relative;
   height: 60px;
+
   .arrow_left {
     cursor: pointer;
     position: absolute;
-    left: 20px;
-    top: 20px;
+    left: calc(30% - 34px);
   }
+
   .arrow_right {
     cursor: pointer;
     position: absolute;
-    right: 20px;
-    top: 20px;
+    right: calc(30% - 34px);
   }
+
   .date {
     border-bottom-color: #aaaaaa;
-    border-bottom: solid 3px;
+    border-bottom: solid 2px;
     font-size: 22px;
     font-weight: 800;
     color: #fff;
@@ -164,14 +172,21 @@ const dateCSS = css`
 const btnCSS = css`
   display: flex;
   justify-content: flex-end;
+  position: absolute;
+  right: 35px;
+
   .edit_btn {
     width: 60px;
     height: 25px;
     border: none;
-    background-color: #ffffff27;
-    color: #d6d6d6;
-    font-size: 14px;
+    font-family: Pretendard, serif;
+    background-color: transparent;
+    color: var(--33cream, #FEFAEB);
+    opacity: 0.5;
+    font-weight: 700;
+    font-size: 16px;
     cursor: pointer;
+
     &:hover {
       background-color: #ffffff40;
     }
