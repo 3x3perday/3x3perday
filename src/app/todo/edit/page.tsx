42c1dac42dddb78b0f39fcd66f1a8df0cc27caad
tankboyy <tankboya@naver.com>
2023-10-23 21:46:52 +0900

    ✨ toda/edit page css 수정

--- src/app/todo/edit/page.tsx
+++ src/app/todo/edit/page.tsx
@@ -1,151 +1,151 @@
 'use client';
 
-import React, { useEffect, useState } from "react";
-import { initializeTodoData, Todo, Todo3x3Model, TodoPage } from '@/types/todo';
-import { css } from '@emotion/react';
-import { TodoItem } from '@/components/Item/TodoItem';
+import React, {useEffect, useState} from "react";
+import {initializeTodoData, Todo, Todo3x3Model, TodoPage} from '@/types/todo';
+import {css} from '@emotion/react';
+import {TodoItem} from '@/components/Item/TodoItem';
 import Image from 'next/image';
 import dayjs from 'dayjs';
-import { useRouter } from 'next/navigation';
-import { Simulate } from 'react-dom/test-utils';
+import {useRouter} from 'next/navigation';
+import {Simulate} from 'react-dom/test-utils';
 
 interface TodoView extends Todo3x3Model {
-    visibleSubTodo: boolean;
+	visibleSubTodo: boolean;
 }
 
 const convertTodoView = (todos: Todo3x3Model[]): TodoView[] => {
-    return todos.map(todo => ({
-        ...todo,
-        visibleSubTodo: false
-    }))
-}
+	return todos.map(todo => ({
+		...todo,
+		visibleSubTodo: false
+	}));
+};
 
 export default function Home() {
-    const router = useRouter()
+	const router = useRouter();
 
-    const [todos, setTodos] = useState<TodoView[]>(convertTodoView(initializeTodoData.todos));
-    const today = dayjs().format('YYYY-MM-DD');
+	const [todos, setTodos] = useState<TodoView[]>(convertTodoView(initializeTodoData.todos));
+	const today = dayjs().format('YYYY-MM-DD');
 
-    const onChangeMainTodo = (e: React.ChangeEvent<HTMLTextAreaElement>, mainTodoId: number) => {
-        const receiveTodos: TodoView[] = [...todos];
-        receiveTodos[mainTodoId].mainTodo.content = e.target.value
-        setTodos(receiveTodos);
-    }
+	const onChangeMainTodo = (e: React.ChangeEvent<HTMLTextAreaElement>, mainTodoId: number) => {
+		const receiveTodos: TodoView[] = [...todos];
+		receiveTodos[mainTodoId].mainTodo.content = e.target.value;
+		setTodos(receiveTodos);
+	};
 
-    const onChangeSubTodo = (e: React.ChangeEvent<HTMLTextAreaElement>, mainTodoId: number, subTodoId: number) => {
-        const receiveTodos: TodoView[] = [...todos];
-        receiveTodos[mainTodoId].subTodos = receiveTodos[mainTodoId].subTodos.map((todo, i) => {
-            if (i === subTodoId) {
-                return {
-                    ...todo,
-                    content: e.target.value,
-                }
-            }
-            return todo;
-        })
-        setTodos(receiveTodos);
-    }
+	const onChangeSubTodo = (e: React.ChangeEvent<HTMLTextAreaElement>, mainTodoId: number, subTodoId: number) => {
+		const receiveTodos: TodoView[] = [...todos];
+		receiveTodos[mainTodoId].subTodos = receiveTodos[mainTodoId].subTodos.map((todo, i) => {
+			if (i === subTodoId) {
+				return {
+					...todo,
+					content: e.target.value,
+				};
+			}
+			return todo;
+		});
+		setTodos(receiveTodos);
+	};
 
-    const onClickToggle = (mainTodoId: number, state: boolean) => {
-        setTodos(prevState => prevState.map(item => {
-            if (item.id === mainTodoId) {
-                item.visibleSubTodo = state;
-            }
-            return item;
-        }))
-    }
+	const onClickToggle = (mainTodoId: number, state: boolean) => {
+		setTodos(prevState => prevState.map(item => {
+			if (item.id === mainTodoId) {
+				item.visibleSubTodo = state;
+			}
+			return item;
+		}));
+	};
 
-    const onClickAdd = (todoId: number) => {
-        const receiveTodos: TodoView[] = [...todos];
-        receiveTodos[todoId].subTodos = receiveTodos[todoId].subTodos.concat([new Todo()]);
-        setTodos(receiveTodos);
-    }
+	const onClickAdd = (todoId: number) => {
+		const receiveTodos: TodoView[] = [...todos];
+		receiveTodos[todoId].subTodos = receiveTodos[todoId].subTodos.concat([new Todo()]);
+		setTodos(receiveTodos);
+	};
 
-    const save = () => {
-        const data = localStorage.getItem("todos");
-        if (data) {
-            const getLocalTodoPageData:TodoPage[] = JSON.parse(data);
-            const todoData:TodoPage|undefined = getLocalTodoPageData.find(val => val.date === today);
-            if(todoData) {
-                const saveData:TodoPage = {
-                    date: today,
-                    todos: todos
-                }
-                const filterTodoData = getLocalTodoPageData.filter(val => val.date !== dayjs().format('YYYY-MM-DD'));
-                localStorage.setItem("todos", JSON.stringify([...filterTodoData, saveData]));
-            } else {
-                localStorage.setItem("todos", JSON.stringify([...getLocalTodoPageData, {
-                    date: dayjs().format('YYYY-MM-DD'),
-                    todos: todos
-                }]));
-            }
-            alert('저장되었습니다.')
-            router.replace('/todo')
-        }
-    }
+	const save = () => {
+		const data = localStorage.getItem("todos");
+		if (data) {
+			const getLocalTodoPageData: TodoPage[] = JSON.parse(data);
+			const todoData: TodoPage | undefined = getLocalTodoPageData.find(val => val.date === today);
+			if (todoData) {
+				const saveData: TodoPage = {
+					date: today,
+					todos: todos
+				};
+				const filterTodoData = getLocalTodoPageData.filter(val => val.date !== dayjs().format('YYYY-MM-DD'));
+				localStorage.setItem("todos", JSON.stringify([...filterTodoData, saveData]));
+			} else {
+				localStorage.setItem("todos", JSON.stringify([...getLocalTodoPageData, {
+					date: dayjs().format('YYYY-MM-DD'),
+					todos: todos
+				}]));
+			}
+			alert('저장되었습니다.');
+			router.replace('/todo');
+		}
+	};
 
-    useEffect(() => {
-        const data = localStorage.getItem("todos")
-        if (data) {
-            const getLocalTodoPageData:TodoPage[] = JSON.parse(data);
-            const todoData:TodoPage|undefined = getLocalTodoPageData.find(val => val.date === today);
-            if(todoData) {
-                const getTodosData = todoData.todos as TodoView[];
-                setTodos(getTodosData)
-            } else {
-                alert('해당 정보를 찾을 수 없습니다.')
-                router.back();
-            }
-        }
-    }, []);
+	useEffect(() => {
+		const data = localStorage.getItem("todos");
+		if (data) {
+			const getLocalTodoPageData: TodoPage[] = JSON.parse(data);
+			const todoData: TodoPage | undefined = getLocalTodoPageData.find(val => val.date === today);
+			if (todoData) {
+				const getTodosData = todoData.todos as TodoView[];
+				setTodos(getTodosData);
+			} else {
+				alert('해당 정보를 찾을 수 없습니다.');
+				router.back();
+			}
+		}
+	}, []);
 
 
+	return (
+		<main css={css`background-color: #292929;
+      padding-top: 63px;`}>
+			<div
+				css={naviContainer}
+			>
+				<Image
+					src="/icon/arrow_left.svg"
+					width={25}
+					height={25}
+					alt="arrow"
+					css={naviLeft}
+					onClick={() => router.back()}
+				/>
+				<div css={naviDate}>{dayjs().format('YYYY-MM-DD')}</div>
+				<p css={naviRight} onClick={save}>
+					완료
+				</p>
+			</div>
+			<div css={inner}>
+				{todos.map((todo, index) => (
+					<div
+						key={todo.id}
+						css={css`
+              margin-bottom: 32px;
+              width: 344px;
+              border: 2px solid var(--33cream, #FEFAEB);
+						`}
+					>
+						<TodoItem
+							id={todo.id}
+							mainTodo={todo.mainTodo}
+							subTodos={todo.subTodos}
+							onChangeMainTodo={onChangeMainTodo}
+							onChangeSubTodo={onChangeSubTodo}
+							visibleSubTodo={todo.visibleSubTodo}
+							onClickToggle={onClickToggle}
+							onClickAddSubTodo={onClickAdd}
+							editable={true}
+						/>
+					</div>
 
-    return (
-        <main css={css`background-color: #292929;
-          padding-top: 63px;`}>
-            <div
-                css={naviContainer}
-            >
-                <Image
-                    src="/icon/arrow_left.png"
-                    width={15}
-                    height={20}
-                    alt="arrow"
-                    css={naviLeft}
-                    onClick={() => router.back()}
-                />
-                <div css={naviDate}>{dayjs().format('YYYY-MM-DD')}</div>
-                <p css={naviRight} onClick={save}>
-                    완료
-                </p>
-            </div>
-            <div css={inner}>
-                {todos.map((todo, index) => (
-                    <div
-                        key={todo.id}
-                        css={css`
-                          margin-bottom: 32px;
-                          width: 344px;
-                        `}
-                    >
-                        <TodoItem
-                            id={todo.id}
-                            mainTodo={todo.mainTodo}
-                            subTodos={todo.subTodos}
-                            onChangeMainTodo={onChangeMainTodo}
-                            onChangeSubTodo={onChangeSubTodo}
-                            visibleSubTodo={todo.visibleSubTodo}
-                            onClickToggle={onClickToggle}
-                            onClickAddSubTodo={onClickAdd}
-                            editable={true}
-                        />
-                    </div>
-
-                ))}
-            </div>
-        </main>
-    );
+				))}
+			</div>
+		</main>
+	);
 }
 const inner = css`
   display: flex;
@@ -158,10 +158,11 @@ const naviContainer = css`
   align-items: center;
   justify-content: center;
   position: relative;
+  padding-bottom: 27px;
 `;
 const naviDate = css`
   border-bottom-color: #aaaaaa;
-  border-bottom: solid 3px;
+  border-bottom: solid 2px;
   font-size: 22px;
   font-weight: 800;
   color: #fff;
@@ -174,11 +175,15 @@ const naviDate = css`
 const naviLeft = css`
   cursor: pointer;
   position: absolute;
-  left: 20px;
+  left: 38px;
 `;
 const naviRight = css`
   cursor: pointer;
+  width: 62px;
+  text-align: center;
   position: absolute;
-  right: 20px;
-  color: #fff;
+  right: 35px;
+  font-size: 16px;
+  color: var(--33cream, #FEFAEB);
+  opacity: 0.5;
 `;
