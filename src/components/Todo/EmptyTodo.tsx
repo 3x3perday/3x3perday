a0d880a5618b92c22bf3f3470abec8247f95df9d
Vespucci95 <donglonk@naver.com>
2023-11-13 23:37:10 +0900

    🎨 마크업 구조 수정

--- src/components/Todo/EmptyTodo.tsx
+++ src/components/Todo/EmptyTodo.tsx
@@ -1,62 +1,21 @@
-import Link from 'next/link';
+import Link, { LinkProps } from 'next/link';
 import { TodoItem } from '@/types/todo';
-import { Icon, ICON_MAP } from '@/components/Icon/Icon';
-import './EmptyTodo.css'
+import { Icon } from '@/components/Icon/Icon';
+import './EmptyTodo.css';
+import FingerButton from '@/components/Button/FingerButton';
 
-
-interface FingerIconProps {
-    name: keyof typeof ICON_MAP;
-    width: string;
-    height: string;
+interface EmptyTodoProps {
+    sortedId: TodoItem['sortedId']
+    href: LinkProps['href']
 }
 
-type FingerIconType = {
-    [key: number]: FingerIconProps;
-}
-
-const ActiveFingerMap: FingerIconType = {
-  1: {
-    name: 'hand-one-active',
-    width: '79px',
-    height: '136.624px',
-  },
-  2: {
-    name: 'hand-two-active',
-    width: '81px',
-    height: '139px',
-  },
-  3: {
-    name: 'hand-three-active',
-    width: '77px',
-    height: '147px',
-  }
-}
-const InActiveFingerMap: FingerIconType = {
-  0: {
-    name: 'hand-one-inactive',
-    width: '79px',
-    height: '136.624px',
-  },
-  1: {
-    name: 'hand-two-inactive',
-    width: '81px',
-    height: '139px',
-  },
-  2: {
-    name: 'hand-three-inactive',
-    width: '77px',
-    height: '147px',
-  }
-}
-
-
-export const EmptyTodo = ({ sortedId }: Pick<TodoItem, "sortedId">) => {
+export const EmptyTodo = ({ sortedId, href }: EmptyTodoProps) => {
   return (
     <Link
-      href="/todo/edit"
+      href={href}
       className="todo-empty-container"
     >
-      <Icon {...InActiveFingerMap[sortedId]} />
+      <FingerButton fingerCount={sortedId} isActive={false} />
       <Icon name="plus" className='plus-icon' />
     </Link>
   )
