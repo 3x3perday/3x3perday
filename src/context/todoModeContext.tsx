81511cd974e5226ffb34dfe4e1e06d65c0a063e4
Vespucci95 <donglonk@naver.com>
2023-11-16 13:16:58 +0900

    ✨ TodoModeContext 추가

---
+++ src/context/todoModeContext.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { createContext, ReactNode } from 'react';
+import { useMode } from '@/hooks/useMode';
+
+export interface TodoMode {
+  mode: 'read' | 'update';
+  setMode: (updateMode: TodoMode['mode']) => void;
+}
+
+export const DEFAULT_VALUE:TodoMode = {
+  mode: 'read',
+  setMode: () => {}
+};
+export const TodoModeContext = createContext<TodoMode>(DEFAULT_VALUE)
+
+export const TodoModeProvider = ({mode, children}:{mode:TodoMode['mode'], children: ReactNode}) => {
+  const value = useMode(mode);
+  return (
+    <TodoModeContext.Provider value={value}>
+      {children}
+    </TodoModeContext.Provider>
+  )
+}
